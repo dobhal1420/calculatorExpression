cb5630e5c0daa765e5407ae3d5aa8d8ffec8287b
Dobhal, Ankit <ankit.dobhal@blackstone.com>
2021-10-20 13:33:13 +0530

    Adding Lambda Entry Endpoint handler.

---
+++ FuturiceCalculator/FuturiceCalculator/LambdaFuturiceEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FuturiceCalculator
+{
+    public class LambdaFuturiceEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
