1678c68546d293703a0f7a39db50df7a7e4f7891
Dobhal, Ankit <ankit.dobhal@blackstone.com>
2021-10-23 23:00:32 +0530

    Making everything asynchronous.

--- FuturiceCalculator/FuturiceCalculatorTests/CalculatorControllerTest.cs
+++ FuturiceCalculator/FuturiceCalculatorTests/CalculatorControllerTest.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace FuturiceCalculatorTests
@@ -12,37 +13,37 @@ namespace FuturiceCalculatorTests
     public class CalculatorControllerTest
     {
         [Fact]
-        public void TestWhenGetAllCalculatorControllerNullQueryParameter()
+        public async Task TestWhenGetAllCalculatorControllerNullQueryParameter()
         {
             //Arrange
             var service = new Mock<IExpressionParserService>();
             var logger = new Mock<ILogger<CalculatorController>>();
             
             service.Setup(x => x.EvaluateExpression(It.IsAny<string>()))
-                .Returns(It.IsAny<decimal>());
+                .ReturnsAsync(It.IsAny<decimal>());
             var controller = new CalculatorController(logger.Object, service.Object);
 
             //Act
-            var result =  controller.Get(null) as StatusCodeResult;
+            var result =  await controller.Get(null).ConfigureAwait(false) as StatusCodeResult;
 
             Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
 
         }
 
         [Fact]
-        public void TestWhenGetAllCalculatorControllerExpressionQueryParameter()
+        public async Task TestWhenGetAllCalculatorControllerExpressionQueryParameterAsync()
         {
             //Arrange
             var service = new Mock<IExpressionParserService>();
             var logger = new Mock<ILogger<CalculatorController>>();
 
             service.Setup(x => x.EvaluateExpression(It.IsAny<string>()))
-                .Returns(It.IsAny<decimal>());
+                .ReturnsAsync(It.IsAny<decimal>());
             var controller = new CalculatorController(logger.Object, service.Object);
             string expressionQuery = "MSoyLTMvNCs1KjYtNyo4KzkvMTA=";
 
             //Act
-            var result = controller.Get(expressionQuery) as OkObjectResult;
+            var result = await controller.Get(expressionQuery).ConfigureAwait(false) as OkObjectResult;
 
             service.Verify(x => x.EvaluateExpression(expressionQuery), Times.Once);
 
